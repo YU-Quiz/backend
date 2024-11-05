152055b4a11754d1ace0ce7185f5b3079e5d0a32
DaeYoung0726 <pdy000726@naver.com>
2024-11-05 18:23:28 +0900

    feat: 메시지 관리를 위한 스케쥴러 처리

--- src/main/java/yuquiz/domain/chatRoom/service/MessageScheduler.java
+++ src/main/java/yuquiz/domain/chatRoom/service/MessageScheduler.java
@@ -1,2 +1,26 @@
-package yuquiz.domain.chatRoom.service;public class MessageScheduler {
+package yuquiz.domain.chatRoom.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import yuquiz.domain.chatRoom.dto.MessageReq;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class MessageScheduler {
+
+    private final ChatMessageService chatMessageService;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void runDailyMessageProcessJob() {
+
+        Map<Long, List<MessageReq>> allMessages = chatMessageService.getAllMessagesGroupedByRoomId();
+
+        allMessages.forEach(chatMessageService::saveMessageInDB);
+    }
 }
