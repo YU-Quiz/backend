273859c47c0851ba1e5b66f1a2e366a2c1175360
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-11-09 20:50:40 +0900

    refact: 필드명 변경

--- src/main/java/yuquiz/domain/studyUser/dto/StudyUserRes.java
+++ src/main/java/yuquiz/domain/studyUser/dto/StudyUserRes.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 public record StudyUserRes (
         Long userId,
-        String username,
+        String nickname,
         StudyRole role,
         LocalDateTime joinedAt
 ){
