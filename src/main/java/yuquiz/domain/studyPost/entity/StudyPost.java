13f9c49a3a68201047017a07c71379b50d858c3e
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-11-16 20:06:06 +0900

    feat: 스터디 게시물 엔티티 추가

---
+++ src/main/java/yuquiz/domain/studyPost/entity/StudyPost.java
@@ -0,0 +1,45 @@
+package yuquiz.domain.studyPost.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import yuquiz.domain.post.entity.Post;
+import yuquiz.domain.study.entity.Study;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class StudyPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    private Study study;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private StudyPostType type;
+
+    @Builder
+    public StudyPost(Study study, Post post, StudyPostType type){
+        this.study = study;
+        this.post = post;
+        this.type = type;
+    }
+}
---
+++ src/main/java/yuquiz/domain/studyPost/entity/StudyPostType.java
@@ -0,0 +1,5 @@
+package yuquiz.domain.studyPost.entity;
+
+public enum StudyPostType {
+    NORMAL, NOTICE
+}
