139c530710ed8b074ecac07be1679cd9735a5593
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-11-19 13:09:09 +0900

    refact: 불필요 어노테이션 삭제

--- src/main/java/yuquiz/domain/studyPost/entity/StudyPost.java
+++ src/main/java/yuquiz/domain/studyPost/entity/StudyPost.java
@@ -1,7 +1,6 @@
 package yuquiz.domain.studyPost.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -14,13 +13,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import yuquiz.domain.post.entity.Post;
 import yuquiz.domain.study.entity.Study;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class StudyPost {
 
