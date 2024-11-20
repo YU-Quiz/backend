d61511986fecae35b0f2e9beb024bfe3bb787429
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-11-20 18:31:29 +0900

    feat: 사용자와 퀴즈를 기준으로 시도여부를 학인하는 기능 추가

--- src/main/java/yuquiz/domain/quiz/repository/CustomTriedQuizRepository.java
+++ src/main/java/yuquiz/domain/quiz/repository/CustomTriedQuizRepository.java
@@ -3,7 +3,10 @@ package yuquiz.domain.quiz.repository;
 import yuquiz.domain.quiz.entity.TriedQuiz;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomTriedQuizRepository {
     List<TriedQuiz> getTriedQuizzes(Long userId);
+
+    Optional<Boolean> getIsSolvedByUser_IdAndQuiz_Id(long userId, long quizId);
 }
--- src/main/java/yuquiz/domain/quiz/repository/CustomTriedQuizRepositoryImpl.java
+++ src/main/java/yuquiz/domain/quiz/repository/CustomTriedQuizRepositoryImpl.java
@@ -5,22 +5,37 @@ import jakarta.persistence.EntityManager;
 import yuquiz.domain.quiz.entity.TriedQuiz;
 
 import java.util.List;
+import java.util.Optional;
 
 import static yuquiz.domain.quiz.entity.QTriedQuiz.triedQuiz;
 
-public class CustomTriedQuizRepositoryImpl implements CustomTriedQuizRepository{
+public class CustomTriedQuizRepositoryImpl implements CustomTriedQuizRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     public CustomTriedQuizRepositoryImpl(EntityManager entityManager) {
         this.jpaQueryFactory = new JPAQueryFactory(entityManager);
     }
+
     @Override
-    public List<TriedQuiz> getTriedQuizzes(Long userId){
+    public List<TriedQuiz> getTriedQuizzes(Long userId) {
 
         return jpaQueryFactory
                 .select(triedQuiz)
                 .from(triedQuiz)
                 .where(triedQuiz.user.id.eq(userId))
                 .fetch();
+    }
+
+    @Override
+    public Optional<Boolean> getIsSolvedByUser_IdAndQuiz_Id(long userId, long quizId) {
+        return Optional.ofNullable(
+                jpaQueryFactory
+                        .select(triedQuiz.isSolved)
+                        .from(triedQuiz)
+                        .where(
+                                triedQuiz.user.id.eq(userId),
+                                triedQuiz.quiz.id.eq(quizId))
+                        .fetchOne()
+        );
     }
 }
