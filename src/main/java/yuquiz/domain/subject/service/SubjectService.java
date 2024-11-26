033dc6857cf311d297c611ebdb992c97faba2a62
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-11-26 18:37:43 +0900

    refeat: 전공 입력 및 과목 코드 로직 수정

--- src/main/java/yuquiz/domain/subject/service/SubjectService.java
+++ src/main/java/yuquiz/domain/subject/service/SubjectService.java
@@ -1,13 +1,14 @@
 package yuquiz.domain.subject.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import yuquiz.common.exception.CustomException;
 import yuquiz.domain.major.entity.Major;
 import yuquiz.domain.major.repository.MajorRepository;
 import yuquiz.domain.subject.dto.SubjectRes;
@@ -32,33 +33,57 @@ public class SubjectService {
                 .collect(Collectors.toList());
     }
 
-    public void addSubject(MultipartFile file) throws IOException{
+    public void addSubject(MultipartFile file) throws IOException {
         Workbook workbook = WorkbookFactory.create(file.getInputStream());
         Sheet sheet = workbook.getSheetAt(0);
 
 
         for (Row row : sheet) {
-            if (row.getRowNum() == 0 || row.getRowNum() == 1 || row.getRowNum() == 2 || row.getRowNum() == 3) {
+            if (row.getRowNum() <= 3) {
                 continue;
             }
-            Major major = majorRepository.findByMajorName(row.getCell(9).getStringCellValue())
-                    .orElseThrow(IOException::new);
+            String majorName = row.getCell(9).getStringCellValue();
 
-            Subject subject = Subject.builder()
-                    .subjectCode(String.valueOf(row.getCell(0).getNumericCellValue()).substring(0,4))
-                    .subjectName(row.getCell(3).getStringCellValue())
-                    .major(major)
-                    .build();
+            Major major = majorRepository.findByMajorName(majorName)
+                    .orElseGet(() -> {
+                        Major newMajor = Major.builder()
+                                .majorName(majorName)
+                                .build();
+                        return majorRepository.save(newMajor);
+                    });
 
-            subjectRepository.save(subject);
+            // 셀에서 subjectCode 가져오기
+            Cell codeCell = row.getCell(0);
+            String subjectCode = null;
 
-            System.out.println(String.valueOf(row.getCell(0).getNumericCellValue()).substring(0,4));
-            System.out.println(row.getCell(3).getStringCellValue());
-            System.out.println(major.getMajorName());
-            System.out.println("-------------------------");
+            if (codeCell != null) {
+                if (codeCell.getCellType() == CellType.NUMERIC) {
+                    // 숫자일 경우
+                    double numericValue = codeCell.getNumericCellValue();
+                    subjectCode = String.valueOf((long) numericValue).substring(0, 4);
+                } else if (codeCell.getCellType() == CellType.STRING) {
+                    // 문자열일 경우
+                    subjectCode = codeCell.getStringCellValue();
+                } else {
+                    throw new IOException();
+                }
+
+                Subject subject = Subject.builder()
+                        .subjectCode(subjectCode)
+                        .subjectName(row.getCell(3).getStringCellValue())
+                        .major(major)
+                        .build();
+
+                subjectRepository.save(subject);
+
+                System.out.println(subjectCode);
+                System.out.println(row.getCell(3).getStringCellValue());
+                System.out.println(major.getMajorName());
+                System.out.println("-------------------------");
+            }
+
+            workbook.close();
         }
-
-        workbook.close();
     }
 
 }
