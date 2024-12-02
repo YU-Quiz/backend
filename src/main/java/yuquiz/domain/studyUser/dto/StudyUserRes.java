7e6deaa96f2d7e133ab59cf8908311795c3cebf7
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-12-02 15:25:40 +0900

    refact: 스터디 사용자 dto 값 수정

--- src/main/java/yuquiz/domain/studyUser/dto/StudyUserRes.java
+++ src/main/java/yuquiz/domain/studyUser/dto/StudyUserRes.java
@@ -8,12 +8,14 @@ import java.time.LocalDateTime;
 public record StudyUserRes (
         Long userId,
         String nickname,
+        String username,
         StudyRole role,
         LocalDateTime joinedAt
 ){
     public static StudyUserRes fromEntity(StudyUser studyUser) {
         return new StudyUserRes(
                 studyUser.getUser().getId(),
+                studyUser.getUser().getNickname(),
                 studyUser.getUser().getUsername(),
                 studyUser.getRole(),
                 studyUser.getJoinedAt()
